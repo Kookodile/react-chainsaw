6eca1eb404f0dffe63657051d65c6028b51ba994
Bastien Deye <basdeye@gmail.com>
2022-07-02 02:16:52 +0200

    First tinkering in app.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-
+import {Box, Button, Paper, Typography} from "@mui/material";
+import "./app.css"
 function App() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <Box m={8} p={2} className="App">
+            <Paper >
+                <Button variant={"contained"}>Bouton</Button>
+                <Typography>Texte</Typography>
+            </Paper>
+        </Box>
     );
-
 }
 
 export default App;
