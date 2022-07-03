3c4f71be7d7ee75b22ac65904560e4e05e62eb40
Bastien Deye <basdeye@gmail.com>
2022-07-04 00:54:36 +0200

    Front -- TodoList -- Create & Delete : Front WIP

--- back/ReactChainsaw.API/Program.cs
+++ back/ReactChainsaw.API/Program.cs
@@ -3,7 +3,18 @@ using ReactChainsaw.API.Database;
 using ReactChainsaw.API.Database.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => {
 
+    options.AddPolicy("cors", cors =>
+    {
+
+        cors.AllowAnyOrigin();
+        cors.AllowAnyHeader();
+        cors.AllowAnyMethod();
+
+    });
+
+});
 // Add services to the container.
 builder.Services.AddDbContextPool<AppDbContext>(option =>
 {
@@ -12,12 +23,15 @@ builder.Services.AddDbContextPool<AppDbContext>(option =>
 });
 
 builder.Services.AddScoped<ToDoRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"]}"));
 
 var app = builder.Build();
+app.UseCors("cors");
+
 var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<AppDbContext>();
 context.Database.EnsureCreated();
 // Configure the HTTP request pipeline.
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,67 @@
 import React from 'react';
-import {Box, Button, Paper, Typography} from "@mui/material";
-import "./app.css"
+import {Box, Button, buttonClasses, Paper, Stack, Typography} from "@mui/material";
+import {ToDo, ToDoApi} from "./api";
+
+const api = new ToDoApi(undefined, "http://localhost:5017");
+
 function App() {
+
+    const [todoTab, setTodoTab] = React.useState<ToDo[]>([]);
+
+    React.useEffect(() => {
+
+        refreshTodo();
+
+        console.log("mount");
+
+    }, []);
+
+    function onClick() {
+        api.addTodos({nom: "randomNom", description: "randomDescription"}).then(refreshTodo);
+
+    }
+
+    function clicDelete(id:number) {
+
+        api.deleteTodos(id).then(refreshTodo);
+
+
+    }
+
+    function refreshTodo() {
+
+        api.getToDos().then((response) => {
+
+            setTodoTab(response.data)
+
+        });
+    }
+
     return (
-        <Box m={8} p={2} className="App">
-            <Paper >
-                <Button variant={"contained"}>Bouton</Button>
-                <Typography>Texte</Typography>
-            </Paper>
-        </Box>
+
+
+        <header>
+            <Button onClick={onClick} variant={"contained"}>Bouton</Button>
+            <Box m={8} p={2} className="App">
+                <Paper style={{padding: "2em"}}>
+
+                    {
+                        todoTab.map(todo => <Stack m={1} direction={"row"}>
+                            <Paper>
+                                <Box p={2} mt={2}>
+                                    <Typography>{todo.nom} {todo.id}</Typography>
+                                    <Button color={"error"} onClick={() => clicDelete(todo.id!)}>Delete</Button>
+                                </Box>
+                            </Paper>
+                        </Stack>)
+                    }
+                    <pre>
+                        {JSON.stringify(todoTab, null, 4)}
+                    </pre>
+                    <Typography>Texte {todoTab[0]?.id}</Typography>
+                </Paper>
+            </Box>
+        </header>
     );
 }
 
